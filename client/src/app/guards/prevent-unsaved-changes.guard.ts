45b9f5c68576aa338bb93b60db42f6988dcdf72f
Muhamad Irgi Fahrezi <fahrezimuhamad28@gmail.com>
2022-04-26 14:40:51 +0700

    End of section 18

--- client/src/app/guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/guards/prevent-unsaved-changes.guard.ts
@@ -7,16 +7,16 @@ import {
 } from '@angular/router';
 import { Observable } from 'rxjs';
 import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+import { ConfirmService } from '../services/confirm.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
-  canDeactivate(component: MemberEditComponent): boolean {
+  constructor(private confirmService: ConfirmService) {}
+  canDeactivate(component: MemberEditComponent): Observable<boolean> | boolean {
     if (component.editForm.dirty) {
-      return confirm(
-        'Are you sure you want to continue? Any unsaved changes will be last'
-      );
+      return this.confirmService.confirm();
     }
     return true;
   }
